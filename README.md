47c8e8e94d65f8258537fa490fdd25eb56ab6931
Michal Handzlik <4hansu@gmail.com>
2024-04-04 14:25:55 +0200

    Add timers

--- README.md
+++ README.md
@@ -3,17 +3,24 @@
 ```
 git clone https://github.com/Cardinal-Cryptography/common-amm
 cd common-amm
-git checkout quick_deploy    ## this branch reduces number of tokens/pairs deployed
 make build-and-wrap-all      ## might require cargo-contract 3.2.0, alternatively use make build-and-wrap-all-dockerized
 npm run compile
 ```
 
+# How to start local node with timers
+
+```
+cd polkadot-sdk              ## polkadot-sdk fork by Cardinal-Cryptography
+git checkout old-timings     ## or wasmi-to-v0.32.0-beta.8
+cargo run --profile production --bin substrate-node -- --dev 2> node.log
+```
+
 # How to deploy Common
 
-Start local chain with default port and then
 ```
 cd common-amm
-npm run deploy-local    ##  prints, among others, the router address needed later
+git checkout quick_deploy    ##  this branch reduces number of tokens/pairs deployed
+npm run deploy-local         ##  prints, among others, the router address needed below
 npm run spawn-dex-data
 ```
 
@@ -21,5 +28,7 @@ npm run spawn-dex-data
 
 Send 50 single-swap trades from Alice (one trade per block):
 ```
-./send_trades.py --router [ADDR] --trades 50
+./send_trades.py --common /path/to/common-amm --node-log /path/to/node.log --router [ADDR] --trades 50
 ```
+
+Please omit `--node-log` if running Substrate version without timers
--- send_trades.py
+++ send_trades.py
@@ -1,17 +1,31 @@
 #!/bin/env python
 
+import re
 from argparse import ArgumentParser
 from os.path import join
 from trade import Dex, Trader, check_file
 
-COMMON_DIR = '/home/hansu/aleph/common'
+
+def get_timings(logfile, skip_first=0):
+    regexp = r'.*call.*Ok.(\d+)'
+    with open(logfile, encoding='utf-8') as f:
+        log = f.read()
+    entries = re.findall(regexp, log)[(2 * skip_first):]
+    res = []
+    for i in range(0, len(entries), 2):
+        res.append(int(entries[i + 1]) - int(entries[i]))
+    return res
+
 
 parser = ArgumentParser(prog='send_trades')
 parser.add_argument('--router', metavar='AccountId', required=True, help='Router address on chain')
 parser.add_argument('--trades', metavar='NUM', type=int, default=10, help='Number of trades to send')
+parser.add_argument('--common', metavar='PATH', type=str, default='./common-amm', help='Path to common-amm')
+parser.add_argument('--node-log', metavar='PATH', type=str, help='Path to the log of running node')
 args = parser.parse_args()
+logfile = check_file(args.node_log) if args.node_log else None
 
-metadata_dir = join(COMMON_DIR, 'artifacts')
+metadata_dir = join(args.common, 'artifacts')
 metadata_files = {
     'router': check_file(metadata_dir, 'router_contract.json'),
     'factory': check_file(metadata_dir, 'factory_contract.json'),
@@ -30,4 +44,8 @@ tr.update_balances()
 tr.set_allowances()
 
 for _ in range(args.trades):
+    if logfile:
+        skip = len(get_timings(logfile, 0))
     tr.trade(1)
+    if logfile:
+        print(f'Walltime {get_timings(logfile, skip)[0]} ns')
